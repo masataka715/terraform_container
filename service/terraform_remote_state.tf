395695e41af5699bff4fe6638261ea5479a5273d
masataka_iMac <cdehilmnrr@gmail.com>
2019-11-30 07:25:04 +0900

    ec2追加

---
+++ service/terraform_remote_state.tf
@@ -0,0 +1,19 @@
+data "terraform_remote_state" "aws_iam" {
+  backend = "s3"
+
+  config = {
+    bucket = "terraform-container"
+    key    = "iam/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    bucket = "terraform-container"
+    key    = "vpc/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+} 
